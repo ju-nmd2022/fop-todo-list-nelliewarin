ecf9990029e6fd3d271bdd7e995b54a86c8fee54
nelliewarin <wane22bh@student.ju.se>
2023-04-05 15:05:30 +0200

    add multiple assignments, check them off

--- todo.js
+++ todo.js
@@ -3,8 +3,29 @@ const addButtonElement = document.getElementById("addButton");
 const listElement = document.getElementById("list");
 
 let assignment;
+let todoArray = [];
 
 addButtonElement.addEventListener("click", () => {
   assignment = inputFieldElement.value;
-  listElement.innerText = assignment;
+  todoArray.push(assignment);
+
+  const todoList = document.createElement("li");
+  const todoParagraph = document.createElement("p");
+
+  todoParagraph.innerText = assignment;
+
+  const checkBox = document.createElement("input");
+  checkBox.type = "radio";
+  checkBox.checked = false;
+
+  checkBox.addEventListener("click", () => {
+    checkBox.checked = true;
+    todoParagraph.style.fontWeight = "normal";
+  });
+
+  todoList.appendChild(checkBox);
+  todoList.appendChild(todoParagraph);
+  listElement.appendChild(todoList);
+
+  inputFieldElement.value = "";
 });
