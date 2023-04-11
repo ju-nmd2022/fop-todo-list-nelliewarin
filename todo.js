bec1d9859e74bbb89b136185b1d2be0e64e8cdb9
nelliewarin <wane22bh@student.ju.se>
2023-04-11 21:12:52 +0200

    locla storage improvements

--- todo.js
+++ todo.js
@@ -2,27 +2,35 @@ const inputFieldElement = document.getElementById("inputField");
 const addButtonElement = document.getElementById("addButton");
 const listElement = document.getElementById("list");
 
-let assignment;
 let todoArray = [];
 
+collectLocalStorage();
+
 addButtonElement.addEventListener("click", () => {
+  createNewAssignment();
+  showAllAssignments();
+  inputFieldElement.value = "";
+});
+
+function createNewAssignment() {
   assignment = inputFieldElement.value;
   todoArray.push(assignment);
+  saveToLocalStorage();
+}
 
+//--------------------------------------------
+
+function showAllAssignments(todo) {
   const todoList = document.createElement("li");
   const todoParagraph = document.createElement("p");
 
-  todoParagraph.innerText = assignment;
+  todoParagraph.innerText = todo;
+
+  //--------------------------------------------
 
   const checkBox = document.createElement("input");
   checkBox.type = "radio";
   checkBox.checked = false;
-
-  const removeBox = document.createElement("img");
-  removeBox.src = "cross.png";
-  removeBox.addEventListener("click", () => {
-    listElement.removeChild(todoList);
-  })
 
   checkBox.addEventListener("click", () => {
     checkBox.checked = true;
@@ -30,10 +38,54 @@ addButtonElement.addEventListener("click", () => {
     todoParagraph.style.textDecoration = "line-through";
   });
 
+  //--------------------------------------------
+
+  const removeBox = document.createElement("img");
+  removeBox.src = "cross.png";
+
+  removeBox.addEventListener("click", () => {
+    clearLocalStorage(todoParagraph.innerText);
+    renderList();
+  });
+
+  //--------------------------------------------
+
   todoList.appendChild(checkBox);
   todoList.appendChild(todoParagraph);
   todoList.appendChild(removeBox);
   listElement.appendChild(todoList);
+}
 
-  inputFieldElement.value = "";
-});
+//--------------------------------------------
+
+function saveToLocalStorage() {
+  let todoArrayStringified = JSON.stringify(todoArray);
+  localStorage.setItem("assigmentsKey", todoArrayStringified);
+}
+
+function collectLocalStorage() {
+  todoArrayStringified = localStorage.getItem("assigmentsKey");
+
+  if (todoArrayStringified != null) {
+    let todoArrayParsed = JSON.parse(todoArrayStringified);
+    todoArray = todoArrayParsed;
+    renderList();
+  }
+}
+
+function clearLocalStorage(todo) {
+  let index = todoArray.findIndex((todoTask) => todoTask === todo);
+  if (index > -1) {
+    todoArray.splice(index, 1);
+  }
+  saveToLocalStorage();
+}
+
+//--------------------------------------------
+
+function renderList() {
+  listElement.innerHTML = "";
+  todoArray.forEach((todo) => {
+    showAllAssignments(todo);
+  });
+}
